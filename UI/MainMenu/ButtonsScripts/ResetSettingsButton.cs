700817609138e9b13e0ad8b84236e976946de85d
Geapen <theogilat@gmail.com>
2026-05-16 16:19:45 +0200

    Aye, the main menu just evolved, didnot thought it would take that long. But hey, now we have proper categories, a player settings saving system, and we can change the resolution. We still need to set the confirmation windows with a timer for the resolution changer. But the rest of the graphics settings should be easy to add. I will not speculate on the bindings change tho... And no cunt leaves until I find out what cunt coded that! Just joking... VAMONOS!

---
+++ UI/MainMenu/ButtonsScripts/ResetSettingsButton.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class ResetSettingsButton : TextMenuButton
+{
+    protected override void Start()
+    {
+        base.Start();
+        if (button != null) button.onClick.AddListener(OnResetClicked);
+    }
+
+    private void OnResetClicked()
+    {
+        if (SettingsManager.Instance != null) SettingsManager.Instance.ResetPendingSettings();
+    }
+}
---
+++ UI/MainMenu/ButtonsScripts/TextMenuButton.cs
@@ -0,0 +1,41 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TextMenuButton : BaseMenuButton
+{
+    [Header("Text Transition")]
+    [SerializeField] private TextMeshProUGUI buttonText;
+
+    public override void OnPointerEnter(PointerEventData eventData)
+    {
+        if (button != null && !button.interactable) return;
+
+        base.OnPointerEnter(eventData); 
+
+        if (buttonText != null && MainMenuUI.Instance != null)
+        {
+            buttonText.color = MainMenuUI.Instance.hoverTextColor;
+        }
+    }
+
+    public override void OnPointerExit(PointerEventData eventData)
+    {
+        base.OnPointerExit(eventData);
+        ResetButtonColor();
+    }
+
+    protected override void OnDisable()
+    {
+        base.OnDisable();
+        ResetButtonColor(); 
+    }
+
+    private void ResetButtonColor()
+    {
+        if (buttonText != null && MainMenuUI.Instance != null)
+        {
+            buttonText.color = MainMenuUI.Instance.normalTextColor;
+        }
+    }
+}
